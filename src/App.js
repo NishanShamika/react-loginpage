ba28d663330e3f12eaeac7dffcdcdbad7964ba16
Nishan Shyamika <79737468+NishanShamika@users.noreply.github.com>
2022-01-25 23:42:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Loginform from './component/LoginForm';
+import LoginForm from './component/LoginForm';
 import Home from './component/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Loginform />} />
+        <Route exact path="/" element={<LoginForm />} />
         <Route exact path="/home" element={<Home />} />
       </Routes>
     </Router>
